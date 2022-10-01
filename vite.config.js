0817a84eab05dec7076543341ba6bcb399a17bc0
Manas <manazharma@gmail.com>
2022-10-01 13:37:19 +0530

    fix: add change

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,8 @@ export default defineConfig({
         "@aws-amplify/ui-react",
         "@aws-amplify/api",
         /^@aws-amplify.*/,
-        "core-js-pure/stable/object/assign.js"
+        "core-js-pure/stable/object/assign.js",
+        /^core-js-pure.*/
       ]
   }}
 })
